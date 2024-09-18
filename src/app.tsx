bf43ceda6c9ad909965944ef702e837df1530e7e
Daniel Afonso <danieljcafonso@gmail.com>
2024-09-18 18:01:55 +0200

    feat: init repo

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import './app.css'
+
+export default function App() {
+  return (
+    <main>
+      <h1 class="text-2xl h-full flex flex-col items-center">
+        This is where the fun begins!
+      </h1>
+    </main>
+  )
+}
